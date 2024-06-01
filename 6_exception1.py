8c7f33602240229010858cedb2f3d7390975b186
Kseniia Ermolaeva <ermolaevaka95@gmail.com>
2024-06-01 11:43:17 +0300

    Решение задач 6-7

--- 6_exception1.py
+++ 6_exception1.py
@@ -4,17 +4,27 @@
 
 Исключения: KeyboardInterrupt
 
-* Перепишите функцию hello_user() из задания while1, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
+* Перепишите функцию hello_user() из задания while1, чтобы она
+  перехватывала KeyboardInterrupt, писала пользователю "Пока!"
   и завершала работу при помощи оператора break
-    
+
+  Напишите функцию hello_user(), которая с помощью функции input() спрашивает
+  пользователя “Как дела?”, пока он не ответит “Хорошо”
+
 """
 
 def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
-    
+    while True:
+        try:
+            user_say = input('Как дела? ')
+        except KeyboardInterrupt:
+            print('Пока!')
+            break
+        if user_say.lower() == 'хорошо':
+            break
+
 if __name__ == "__main__":
     hello_user()
